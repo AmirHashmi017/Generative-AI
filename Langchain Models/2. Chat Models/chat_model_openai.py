4ca8cc551ccaa093582a57aec21e5bfb31f961c1
Amir Hashmi <amirhashmi017@gmail.com>
2025-10-24 11:57:25 +0500

    Document Similarity using OpenAI Embedding Model

--- Langchain Models/2. Chat Models/chat_model_openai.py
+++ Langchain Models/2. Chat Models/chat_model_openai.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# temperature tells the creativity amount in answer that how ditinguish anser should be , where
-# max_completion_tokens define the maximum number of words there should be in response
+# If temprature is 0 means we get same response for same prompt everytime but if 
+# we chnage that to 0.5 or 1 or 1.5 we will get diffferent response for same input and greater
+# the temprature scre more distinguished response there will be
+
 chat_model=ChatOpenAI(model='gpt-4',temperature=1.5,max_completion_tokens=10)
 
 result= chat_model.invoke('Write 5 lines on cricket')
