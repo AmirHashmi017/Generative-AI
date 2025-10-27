e97803eaf50006daa660ce3fd65cf1c0cfa11bd5
Amir Hashmi <amirhashmi017@gmail.com>
2025-10-27 11:22:08 +0500

    Langchain json Output Parser

--- Langchain Output Parsers/json_output_parser.py
+++ Langchain Output Parsers/json_output_parser.py
@@ -1,26 +1,22 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 
 load_dotenv()
 
-llm= HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
-    task="chat-completion"
-)
 
-model= ChatHuggingFace(llm=llm)
+model= ChatOpenAI(model='gpt-5')
 parser= JsonOutputParser()
 
 prompt_template= PromptTemplate(
     template='''
 Git me the name, age, city of a fictional person \n {format_instruction}''',
 input_variables=[],
-partial_variables= {'format_instruction':parser.get_format_instructions}
+partial_variables= {'format_instruction':parser.get_format_instructions()}
 )
 
-prompt =prompt_template.format()
+chain= prompt_template | model | parser
 
-result= model.invoke(prompt)
-print(result.content)
+result= chain.invoke({})
+print(result)
