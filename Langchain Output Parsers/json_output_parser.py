649749370bcbded76754e57f23dd6d7c204ede86
Amir Hashmi <amirhashmi017@gmail.com>
2025-10-27 11:26:36 +0500

    LangChain str and json Output Parsers

--- Langchain Output Parsers/json_output_parser.py
+++ Langchain Output Parsers/json_output_parser.py
@@ -1,12 +1,14 @@
-from langchain_openai import ChatOpenAI
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 
 load_dotenv()
 
+llm= HuggingFaceEndpoint(repo_id="google/gemma-2-2b-it",
+                         task= "text-generation")
 
-model= ChatOpenAI(model='gpt-5')
+model= ChatHuggingFace(llm=llm)
 parser= JsonOutputParser()
 
 prompt_template= PromptTemplate(
