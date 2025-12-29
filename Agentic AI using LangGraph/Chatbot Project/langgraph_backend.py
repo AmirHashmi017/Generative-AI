9d3c4a89cd4344735181c050bc67b1fbcdef8d02
Amir Hashmi <amirhashmi017@gmail.com>
2025-12-29 17:23:32 +0500

    Chatbot using LangGraph

--- Agentic AI using LangGraph/Chatbot Project/langgraph_backend.py
+++ Agentic AI using LangGraph/Chatbot Project/langgraph_backend.py
@@ -3,7 +3,7 @@ from typing import List,TypedDict,Annotated
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,7 +18,7 @@ def chat_node(state: ChatState):
     response= llm.invoke(messages)
     return{"messages":AIMessage(content=response.content)}
 
-checkpointer= MemorySaver()
+checkpointer= InMemorySaver()
 graph= StateGraph(ChatState)
 
 graph.add_node("chat_node",chat_node)
@@ -26,15 +26,3 @@ graph.add_edge(START,"chat_node")
 graph.add_edge("chat_node",END)
 
 chatbot= graph.compile(checkpointer=checkpointer)
-
-thread_id= '1'
-
-while True:
-    user_message= input("Type Here: ")
-    print(f"User: {user_message}")
-    if user_message.strip().lower() in ['exit','quit','bye']:
-        break
-    
-    config= {'configurable':{'thread_id':thread_id}}
-    response= chatbot.invoke({"messages":[HumanMessage(content=user_message)]},config=config)
-    print(f"AI: {response['messages'][-1].content}")
