e1e9b5059b09f31795ccf3aaed56bf5d365a20f7
Amir Hashmi <amirhashmi017@gmail.com>
2026-01-22 17:57:44 +0500

    Resume Chat and Conversation History in ChatBot using LangGraph

--- Agentic AI using LangGraph/Chatbot Project/streamlit_frontend.py
+++ Agentic AI using LangGraph/Chatbot Project/streamlit_frontend.py
@@ -25,9 +25,8 @@ def load_conversation(thread_id):
     return state.values.get("messages", [])
 
 def generate_chat_title(question):
-    return question[:25] if len(question)>25 else question
+    return question[:30] if len(question)>30 else question
 
-is_streaming=False
 if 'message_history' not in st.session_state:
     st.session_state['message_history']=[]
 
@@ -55,7 +54,7 @@ st.sidebar.title("LangGraph Chatbot")
 
 if st.sidebar.button("New Chat"):
     reset_chat()
-    st.rerun()
+
 
 st.sidebar.header("My Conversations")
 
@@ -74,42 +73,22 @@ for thread_id in st.session_state['chat_threads']:
             temp_messages.append({'role': role, 'content': message.content})
 
         st.session_state['message_history'] = temp_messages
-        st.rerun()
 
-if(not is_streaming):
-    for message in st.session_state['message_history']:
-        with st.chat_message(message["role"]):
-            st.text(message["content"])
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
 
 if user_input:
-    is_streaming=True
     st.session_state['message_history'].append({'role':'user','content':user_input})
     with st.chat_message('user'):
         st.text(user_input)   
-    # with st.chat_message('assistant'):
-    #     response= chatbot.invoke(
-    #         {'messages':[HumanMessage(content=user_input)]},
-    #         config=config
-    #     )
-    #     ai_message = ""
-    #     if 'messages' in response:
-    #         ai_messages = [msg.content for msg in response['messages'] if isinstance(msg, AIMessage)]
-    #         if ai_messages:
-    #             ai_message = ai_messages[-1]
-    #     with st.chat_message('assistant'):
-    #         st.text(ai_message)
-        # ai_message= st.write_stream(
-        #     message_chunk.content for message_chunk,metadata in chatbot.stream(
-        #         {'messages':[HumanMessage(content=user_input)]},
-        #         config=config,
-        #         stream_mode= "messages"
-        #     )
-        # )
+   
     with st.chat_message("assistant"):
         placeholder = st.empty()
         full_response = ""
         last_len = 0
-    
+
         for chunk, _ in chatbot.stream(
             {"messages": [HumanMessage(content=user_input)]},
             config=config,
@@ -118,12 +97,11 @@ if user_input:
             if isinstance(chunk, AIMessage):
                 if len(chunk.content) <= last_len:
                     continue
-                
+
                 full_response = chunk.content
                 last_len = len(full_response)
                 placeholder.markdown(full_response)
 
     st.session_state['message_history'].append({'role':'assistant','content':full_response})
-    is_streaming=False
 
     
